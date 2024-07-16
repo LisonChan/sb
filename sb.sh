77555eb18a25c73f5d86f51120cb3de1c1fc81e5
LisonChan <38126452+LisonChan@users.noreply.github.com>
2024-07-16 22:43:08 +0800

    Update and rename 1.sh to sb.sh

--- sb.sh
+++ sb.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# 添加 testing 仓库
 echo "@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+
+# 更新软件包列表
+apk update
+
+# 安装 sing-box
 apk add sing-box@testing
+
+# 安装 vim
 apk add vim
