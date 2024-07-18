80515d95c7491159c6acf0a6fe663c15dc51d294
LisonChan <38126452+LisonChan@users.noreply.github.com>
2024-07-18 19:43:53 +0800

    Update sb.sh

--- sb.sh
+++ sb.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# 添加 testing 仓库
-echo "@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
-
-# 更新软件包列表
-apk update
-
 # 安装 sing-box
-apk add sing-box@testing
+echo "@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && apk update && apk add sing-box
 
 # 设置自启
 cat > /etc/init.d/singbox << EOF
@@ -36,10 +30,4 @@ stop() {
 EOF
 
 # 给 singbox 启动脚本赋予执行权限
-chmod +x /etc/init.d/singbox
-
-# 将 singbox 服务添加到启动列表
-rc-update add singbox default
-
-# 启动 singbox 服务
-service singbox start
+chmod +x /etc/init.d/singbox && rc-update add singbox default && service singbox start
