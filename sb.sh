978cc206b8bbd174d626c084b3a61d03993d122e
LisonChan <38126452+LisonChan@users.noreply.github.com>
2024-07-27 20:17:34 +0800

    Update sb.sh

--- sb.sh
+++ sb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 安装 sing-box
-echo "@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && apk update && apk add sing-box
+echo "@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && apk update && apk add sing-box@testing
 
 # 设置自启
 cat > /etc/init.d/singbox << EOF
